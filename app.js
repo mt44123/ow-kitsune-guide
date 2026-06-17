87e8d955b7d2d9e1c3d5987898b0f0998809ee5a
mt44123 <mayowanai.kohitsuji@gmail.com>
2026-06-18 04:19:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-document.body.insertAdjacentHTML(
-  "beforeend",
-  "<p style='color:#f99e1a;'>app.js loaded ✅</p>"
-);
+const API_URL = "https://script.google.com/macros/s/AKfycbxLKxHCKL4IxWOsPNaREXHZ6ogng64n3tbC3RPzaKYfkybQpC7kI6rCgRY7JzAxJz6P/exec";
+
+fetch(API_URL + "?view=new")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      "<pre style='color:#f99e1a; white-space:pre-wrap;'>" +
+      JSON.stringify(data.players.slice(0, 3), null, 2) +
+      "</pre>"
+    );
+  });
