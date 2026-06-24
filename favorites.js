e5d671b90e92409caf5460e5a5b2239e5efced88
mt44123 <mayowanai.kohitsuji@gmail.com>
2026-06-24 18:22:40 +0900

    Update favorites.js

--- favorites.js
+++ favorites.js
@@ -1,1 +1,26 @@
+function getFavorites_() {
+  return JSON.parse(
+    localStorage.getItem("favorites") || "[]"
+  );
+}
 
+function isFavorite_(name) {
+  return getFavorites_().includes(name);
+}
+
+function toggleFavorite_(name) {
+  const favs = getFavorites_();
+
+  const index = favs.indexOf(name);
+
+  if (index >= 0) {
+    favs.splice(index, 1);
+  } else {
+    favs.push(name);
+  }
+
+  localStorage.setItem(
+    "favorites",
+    JSON.stringify(favs)
+  );
+}
