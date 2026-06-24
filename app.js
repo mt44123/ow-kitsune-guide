d2394b4f2c24fc6bbb4fc476ae6884d982f4a6d4
mt44123 <mayowanai.kohitsuji@gmail.com>
2026-06-24 18:23:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -128,33 +128,6 @@ const clipLastUpdated = {};
 let currentTeamName = "";
 let currentRegionName = null;
 
-function getFavorites_() {
-  return JSON.parse(
-    localStorage.getItem("favorites") || "[]"
-  );
-}
-
-function isFavorite_(name) {
-  return getFavorites_().includes(name);
-}
-
-function toggleFavorite_(name) {
-  const favs = getFavorites_();
-
-  const index = favs.indexOf(name);
-
-  if (index >= 0) {
-    favs.splice(index, 1);
-  } else {
-    favs.push(name);
-  }
-
-  localStorage.setItem(
-    "favorites",
-    JSON.stringify(favs)
-  );
-}
-
 let birthdaysCache = null;
 let birthdaysCacheTime = 0;
 const BIRTHDAYS_CLIENT_CACHE_MS = 6 * 60 * 60 * 1000;
